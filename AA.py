09eeec647dd865e6af35ceeee3064cddaa29db2b
sdj3261 <63071967+sdj3261@users.noreply.github.com>
2021-08-24 05:22:52 +0900

    인프런

--- AA.py
+++ AA.py
@@ -4,7 +4,7 @@ T = int(input())
 for t in range(T) :
     n,s,e,k = map(int,input().split())
     temp = []
-    a = list(map(int,input().split())) # 한줄을 int형 리스트로 읽기
+    a = list(map(int,input().split())) # 한줄을 int형   리스트로 읽기
     a = a[s-1:e]
     a.sort()
     print("#%d %d" %(t+1, a[k-1])) # %d를 이용하여 출력
